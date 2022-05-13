b951e1aa7ecb16fec4a3ee2cc613de236312c2da
Prince Odia <princeodia@gmail.com>
2022-05-13 04:33:15 +0100

    Add project

---
+++ app/src/main/java/com/princeoprince/firstapp/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.princeoprince.firstapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import com.princeoprince.firstapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            val textDisplayedValue = findViewById<TextView>(R.id.textDisplayedValue)
+            val originalValue = textDisplayedValue.text.toString().toInt()
+            val newValue = originalValue * 2
+            textDisplayedValue.text = newValue.toString()
+            Snackbar.make(view, "Value $originalValue changed to $newValue",
+                Snackbar.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
